33357353ee53968c6029f9e47e7e833c05e1db48
Laurynas <31208060+Skywalker190@users.noreply.github.com>
2018-12-25 16:20:59 +0100

    playing with BULMA

--- reactaxios/src/App.js
+++ reactaxios/src/App.js
@@ -69,8 +69,8 @@ componentDidMount(){
        <ul>
         {users.filter(searchingFor(this.state.term)).map(user =>
           <li key={user.id}>
-              <h2>User {user.id}:</h2>
-              <h3><strong>Name:</strong> {user.first_name} <br></br>
+              <h2 className="title">User {user.id}:</h2>
+              <h3 className="subtitle"><strong>Name:</strong> {user.first_name} <br></br>
                 <strong>Last Name:</strong> {user.last_name}</h3> 
               <figure className="image is-128x128">
                 <img alt="avatar" className="is-rounded" src={user.avatar}></img>
