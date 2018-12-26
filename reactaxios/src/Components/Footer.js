330a1ed6291d81fadea30e7802ca52d8683187f3
Laurynas <31208060+Skywalker190@users.noreply.github.com>
2018-12-26 14:05:12 +0100

    Update Footer.js

--- reactaxios/src/Components/Footer.js
+++ reactaxios/src/Components/Footer.js
@@ -11,7 +11,7 @@ import 'bulma/css/bulma.css';
   <div className="content has-text-centered">
     <p>
       <strong>React</strong> by <a href="https://github.com/LaurynasNaujokas/React-axios">Laurynas</a>. Just for fun and educational purposes
-      <a href="http://opensource.org/licenses/mit-license.php">  MIT</a>. The website content
+      <a href="https://github.com/LaurynasNaujokas/React-axios">  MIT</a>. The website content
       is made for fun <a href="https://github.com/LaurynasNaujokas/React-axios">2018 Denmark</a>.
     </p>
   </div>
